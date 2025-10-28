3a7886fd7ba384b389f3108778c096bae656538d
Manikandan0-0 <manikandan.s.imp@gmail.com>
2025-10-28 11:13:30 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AdminDashboard from './components/mvpblocks'
+
+const App = () => {
+  return (
+    <>
+    < AdminDashboard />
+    </>
+  )
+}
+
+export default App
